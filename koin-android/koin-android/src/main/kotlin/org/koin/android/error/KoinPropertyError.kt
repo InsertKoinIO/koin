e8aab261c79000fa6462f0094851a3746749ba0a
Arnaud Giuliani <agiuliani@ekito.fr>
2017-09-11 09:56:51 +0200

    [Android] property injection

---
+++ koin-android/koin-android/src/main/kotlin/org/koin/android/error/KoinPropertyError.kt
@@ -0,0 +1,6 @@
+package org.koin.android.error
+
+/**
+ * Error while getting property
+ */
+class KoinPropertyError(msg : String) : Exception(msg)
--- koin-android/koin-android/src/main/kotlin/org/koin/android/ext/android/app/AndroidExt.kt
+++ koin-android/koin-android/src/main/kotlin/org/koin/android/ext/android/app/AndroidExt.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import org.koin.KoinContext
 import org.koin.android.KoinContextAware
 import org.koin.android.error.KoinApplicationException
+import org.koin.android.error.KoinPropertyError
 import kotlin.reflect.KClass
 
 
@@ -32,7 +33,7 @@ fun Activity.getKoin(): KoinContext = this.application.getKoin()
 /**
  * Inject current dependency
  */
-inline fun <reified T> Activity.get(name : String = ""): T = getKoin().get(name)
+inline fun <reified T> Activity.get(name: String = ""): T = getKoin().get(name)
 
 /**
  * Release scope instances
@@ -42,13 +43,22 @@ fun Activity.release(vararg scopeClasses: KClass<*>) = getKoin().release(*scopeC
 /**
  * inject lazily given dependency for Activity
  */
-inline fun <reified T> Activity.inject(name : String = ""): Lazy<T> = lazy { getKoin().get<T>(name) }
+inline fun <reified T> Activity.inject(name: String = "") = lazy { getKoin().get<T>(name) }
 
 /**
  * inject lazily given dependency for Activity - can be null
  */
-inline fun <reified T> Activity.injectOrNull(name : String = ""): Lazy<T?> = lazy { getKoin().getOrNull<T>(name) }
+inline fun <reified T> Activity.injectOrNull(name: String = "") = lazy { getKoin().getOrNull<T>(name) }
 
+/**
+ * inject lazily given property for Activity
+ */
+inline fun <reified T> Activity.property(key: String) = lazy { getKoin().getProperty<T>(key).takeIf { key.isNotEmpty() } ?: KoinPropertyError("Can't inject empty property") }
+
+/**
+ * inject lazily given property for Activity or return null
+ */
+inline fun <reified T> Activity.propertyOrNll(key: String) = lazy { getKoin().getPropertyOrNull<T>(key) }
 
 /* Fragment */
 
@@ -60,22 +70,32 @@ fun Fragment.getKoin(): KoinContext = activity.getKoin()
 /**
  * Inject current dependency
  */
-inline fun <reified T> Fragment.get(name : String = ""): T = getKoin().get(name)
+inline fun <reified T> Fragment.get(name: String = ""): T = getKoin().get(name)
 
 /**
  * inject lazily given dependency for Fragment
  */
-inline fun <reified T> Fragment.inject(name : String = ""): Lazy<T> = lazy { getKoin().get<T>(name) }
+inline fun <reified T> Fragment.inject(name: String = ""): Lazy<T> = lazy { getKoin().get<T>(name) }
 
 /**
  * inject lazily given dependency for Fragment - can be null
  */
-inline fun <reified T> Fragment.injectOrNull(name : String = ""): Lazy<T?> = lazy { getKoin().getOrNull<T>(name) }
+inline fun <reified T> Fragment.injectOrNull(name: String = ""): Lazy<T?> = lazy { getKoin().getOrNull<T>(name) }
 
 /**
  * Release scope instances
  */
 fun Fragment.release(vararg scopeClasses: KClass<*>) = getKoin().release(*scopeClasses)
+
+/**
+ * inject lazily given property for Activity
+ */
+inline fun <reified T> Fragment.property(key: String) = lazy { getKoin().getProperty<T>(key).takeIf { key.isNotEmpty() } ?: KoinPropertyError("Can't inject empty property") }
+
+/**
+ * inject lazily given property for Activity or return null
+ */
+inline fun <reified T> Fragment.propertyOrNll(key: String) = lazy { getKoin().getPropertyOrNull<T>(key) }
 
 
 /* Service */
@@ -88,17 +108,27 @@ fun Service.getKoin() = this.application.getKoin()
 /**
  * Inject current dependency
  */
-inline fun <reified T> Service.get(name : String = ""): T = getKoin().get(name)
+inline fun <reified T> Service.get(name: String = ""): T = getKoin().get(name)
 
 /**
  * inject lazily given dependency for Activity
  */
-inline fun <reified T> Service.inject(name : String = ""): Lazy<T> = lazy { getKoin().get<T>(name) }
+inline fun <reified T> Service.inject(name: String = ""): Lazy<T> = lazy { getKoin().get<T>(name) }
 
 /**
  * inject lazily given dependency for Activity - can be null
  */
-inline fun <reified T> Service.injectOrNull(name : String = ""): Lazy<T?> = lazy { getKoin().getOrNull<T>(name) }
+inline fun <reified T> Service.injectOrNull(name: String = ""): Lazy<T?> = lazy { getKoin().getOrNull<T>(name) }
+
+/**
+ * inject lazily given property for Activity
+ */
+inline fun <reified T> Service.property(key: String) = lazy { getKoin().getProperty<T>(key).takeIf { key.isNotEmpty() } ?: KoinPropertyError("Can't inject empty property") }
+
+/**
+ * inject lazily given property for Activity or return null
+ */
+inline fun <reified T> Service.propertyOrNll(key: String) = lazy { getKoin().getPropertyOrNull<T>(key) }
 
 /**
  * Release scope instances
