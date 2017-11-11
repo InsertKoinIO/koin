bddebd727c8cb001b534b6320a1a7c0451a2f18e
Arnaud Giuliani <agiuliani@ekito.fr>
2017-11-11 23:19:56 +0100

    [sample] debug log

--- samples/android-weatherapp/app/src/main/kotlin/org/koin/sampleapp/MainApplication.kt
+++ samples/android-weatherapp/app/src/main/kotlin/org/koin/sampleapp/MainApplication.kt
@@ -1,10 +1,13 @@
 package org.koin.sampleapp
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
 import com.joanzapata.iconify.Iconify
 import com.joanzapata.iconify.fonts.WeathericonsModule
+import org.koin.Koin
 import org.koin.android.ext.android.bindString
 import org.koin.android.ext.android.startAndroidContext
+import org.koin.android.logger.AndroidLogger
 import org.koin.sampleapp.di.WeatherModule
 import org.koin.sampleapp.di.weatherAppModules
 
@@ -14,6 +17,10 @@ class MainApplication : Application() {
         super.onCreate()
 
         startAndroidContext(this, weatherAppModules())
+        val isDebug = (0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE)
+        if (isDebug) {
+            Koin.logger = AndroidLogger()
+        }
 
         // bind R.string.server_url to Koin WeatherModule.SERVER_URL
         bindString(R.string.server_url, WeatherModule.SERVER_URL)
