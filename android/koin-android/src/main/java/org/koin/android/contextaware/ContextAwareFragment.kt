e54dd17d04025593e38f52a9f8e56f3fc746e282
Arnaud Giuliani <agiuliani@ekito.fr>
2017-11-16 14:33:13 +0100

    [android] context aware components / drop configuration

--- android/koin-android/src/main/java/org/koin/android/contextaware/ContextAwareFragment.kt
+++ android/koin-android/src/main/java/org/koin/android/contextaware/ContextAwareFragment.kt
@@ -1,6 +1,7 @@
 package org.koin.android.contextaware
 
 import android.support.v4.app.Fragment
+import org.koin.Koin
 import org.koin.android.ext.android.releaseContext
 
 /**
@@ -10,15 +11,21 @@ import org.koin.android.ext.android.releaseContext
  *
  * @author Arnaud Giuliani
  */
-abstract class ContextAwareFragment : Fragment() {
-
-    /**
-     * Koin sub context name
-     */
-    abstract val contextName: String
+abstract class ContextAwareFragment(override val contextName: String, override val contextDrop: ContextDropMethod = ContextAwareConfig.defaultContextDrop) : ContextAwareComponent, Fragment() {
 
     override fun onPause() {
-        releaseContext(contextName)
+        if (contextDrop == ContextDropMethod.OnPause) {
+            Koin.logger.log("Fragment ContextAware - drop on onPause")
+            releaseContext(contextName)
+        }
         super.onPause()
+    }
+
+    override fun onDestroy() {
+        if (contextDrop == ContextDropMethod.OnDestroy) {
+            Koin.logger.log("Fragment ContextAware - drop on onDestroy")
+            releaseContext(contextName)
+        }
+        super.onDestroy()
     }
 }
