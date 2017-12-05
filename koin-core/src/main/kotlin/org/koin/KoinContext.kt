93aa06203ab5853d3ed8f3dc3ecd7a4677716864
Arnaud Giuliani <agiuliani@ekito.fr>
2017-12-06 00:34:55 +0100

    [core] sync

--- koin-core/src/main/kotlin/org/koin/KoinContext.kt
+++ koin-core/src/main/kotlin/org/koin/KoinContext.kt
@@ -45,7 +45,7 @@ class KoinContext(val beanRegistry: BeanRegistry, val propertyResolver: Property
     /**
      * Resolve a dependency for its bean definition
      */
-    fun <T> resolveInstance(clazz: KClass<*>, resolver: () -> BeanDefinition<*>): T {
+    fun <T> resolveInstance(clazz: KClass<*>, resolver: () -> BeanDefinition<*>): T = synchronized(this) {
         logger.log("[Context] Resolve [${clazz.java.canonicalName}]")
 
         if (resolutionStack.contains(clazz)) {
