5263dbda83a745bb807aa09948f7521770fdbd5e
Fredy Mederos <fmfredd@gmail.com>
2018-01-04 12:47:11 -0500

    Adding extensions functions to inject lazily given viewModel for Android Fragment and FragmentActivity

--- android/koin-android-architecture/src/main/java/org/koin/android/architecture/ext/AndroidExt.kt
+++ android/koin-android-architecture/src/main/java/org/koin/android/architecture/ext/AndroidExt.kt
@@ -8,14 +8,36 @@ import android.support.v4.app.FragmentActivity
 import org.koin.standalone.KoinComponent
 
 
+/**
+ * Retrieve given ViewModel for Android Fragment
+ */
 inline fun <reified T : ViewModel> Fragment.getViewModel(): T {
     val vm = ViewModelProvider(ViewModelStores.of(this), KoinFactory)
     return vm.get(T::class.java)
 }
 
+/**
+ * Retrieve given ViewModel for Android FragmentActivity
+ */
 inline fun <reified T : ViewModel> FragmentActivity.getViewModel(): T {
     val vm = ViewModelProvider(ViewModelStores.of(this), KoinFactory)
     return vm.get(T::class.java)
+}
+
+/**
+ * inject lazily given viewModel for Android Fragment
+ */
+inline fun <reified T : ViewModel> Fragment.injectViewModel(): Lazy<T> = lazy {
+    val vm = ViewModelProvider(ViewModelStores.of(this), KoinFactory)
+    vm.get(T::class.java)
+}
+
+/**
+ * inject lazily given viewModel for Android FragmentActivity
+ */
+inline fun <reified T : ViewModel> FragmentActivity.injectViewModel(): Lazy<T> = lazy {
+    val vm = ViewModelProvider(ViewModelStores.of(this), KoinFactory)
+    vm.get(T::class.java)
 }
 
 object KoinFactory : ViewModelProvider.Factory, KoinComponent {
