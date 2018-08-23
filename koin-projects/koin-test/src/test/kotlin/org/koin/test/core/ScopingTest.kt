9d5ec52326e800c7a4dda9241113e329da1e3246
Arnaud Giuliani <agiuliani@ekito.fr>
2018-08-23 14:56:58 +0200

    add tests

---
+++ koin-projects/koin-test/src/test/kotlin/org/koin/test/core/ScopingTest.kt
@@ -0,0 +1,27 @@
+package org.koin.test.core
+
+import org.junit.Assert
+import org.junit.Test
+import org.koin.dsl.module.module
+import org.koin.standalone.StandAloneContext
+import org.koin.standalone.get
+import org.koin.test.AutoCloseKoinTest
+
+class ScopingTest : AutoCloseKoinTest() {
+
+    @Test
+    fun `test with scope`() {
+        val scopeName = "MyScope"
+        StandAloneContext.loadKoinModules(
+            module(scopeName) {
+                single { "Example String we want!" }
+            },
+            module("SomeOtherScopeOverHere") {
+                single { "DON'T GIVE ME THIS!" }
+            }
+        )
+
+        val result = get<String>(module = scopeName)
+        Assert.assertNotNull(result)
+    }
+}
