9356535f54aa9d2f57d54515f5a6afee5a304287
Fredy Mederos <fmfredd@gmail.com>
2018-08-25 23:19:44 -0400

    Including a couple of fragments and viewModels in sample-navigation project to show an example of sharing viewModels between fragments and not with activity using the new "from" keyword

--- koin-projects/examples/android-navigation/src/main/java/org/koin/sample/navigation/AppModule.kt
+++ koin-projects/examples/android-navigation/src/main/java/org/koin/sample/navigation/AppModule.kt
@@ -1,6 +1,7 @@
 package org.koin.sample.navigation
 
 import org.koin.android.viewmodel.experimental.builder.viewModel
+import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 // uses experimental experimental builder
@@ -8,4 +9,5 @@ val appModule = module {
     viewModel<FirstViewModel>()
     viewModel<SecondViewModel>()
     viewModel<ThirdViewModel>()
+    viewModel { (sharedValue: String) -> FourFiveSharedViewModel(sharedValue) }
 }
