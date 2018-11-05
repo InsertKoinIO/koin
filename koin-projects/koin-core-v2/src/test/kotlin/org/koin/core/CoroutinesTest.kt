c5245c1eae1b32a215af9903e795fd769566034d
Arnaud Giuliani <agiuliani@ekito.fr>
2018-11-05 10:05:57 +0100

    coroutines test

--- koin-projects/koin-core-v2/src/test/kotlin/org/koin/core/CoroutinesTest.kt
+++ koin-projects/koin-core-v2/src/test/kotlin/org/koin/core/CoroutinesTest.kt
@@ -1,12 +1,62 @@
 package org.koin.core
 
+import kotlinx.coroutines.*
+import org.junit.Assert
 import org.junit.Test
+import org.koin.Simple
+import org.koin.dsl.koinApplication
+import org.koin.dsl.module
+import org.koin.test.getDefinition
+import org.koin.test.hasBeenCreated
+import kotlin.random.Random
 
 class CoroutinesTest {
 
     @Test
-    fun `coroutines start`() {
-        TODO()
+    fun `KoinApp with coroutines gets`() = runBlocking {
+        val app = koinApplication {
+            loadModules(
+                module {
+                    single { Simple.ComponentA() }
+                    single { Simple.ComponentB(get()) }
+                    single { Simple.ComponentC(get()) }
+                })
+        }.start()
+        val koin = app.koin
+
+        val jobs = arrayListOf<Deferred<*>>()
+        jobs.add(async {
+            randomSleep()
+            koin.get<Simple.ComponentA>()
+        })
+        jobs.add(async {
+            randomSleep()
+            koin.get<Simple.ComponentB>()
+        })
+        jobs.add(async {
+            randomSleep()
+            koin.get<Simple.ComponentC>()
+        })
+        jobs.awaitAll()
+
+        val a = app.getDefinition(Simple.ComponentA::class)!!
+        val b = app.getDefinition(Simple.ComponentA::class)!!
+        val c = app.getDefinition(Simple.ComponentA::class)!!
+
+        Assert.assertTrue(a.hasBeenCreated())
+        Assert.assertTrue(b.hasBeenCreated())
+        Assert.assertTrue(c.hasBeenCreated())
+
+        app.stop()
+
+    }
+
+    val MAX_TIME = 1000L
+
+    private suspend fun randomSleep() {
+        val timer = Random.nextLong(MAX_TIME)
+        println("thread sleep  $timer")
+        delay(timer)
     }
 
 }
