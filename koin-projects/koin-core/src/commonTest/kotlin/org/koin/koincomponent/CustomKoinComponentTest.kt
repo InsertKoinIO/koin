ce0587d71eb782d6e5ee07dbb891e6212e248047
Dennis Rieks <dennisrieks@googlemail.com>
2019-07-04 23:03:07 +0200

    MP step 3: fixed CustomKoinComponentTest

--- koin-projects/koin-core/src/commonTest/kotlin/org/koin/koincomponent/CustomKoinComponentTest.kt
+++ koin-projects/koin-core/src/commonTest/kotlin/org/koin/koincomponent/CustomKoinComponentTest.kt
@@ -10,17 +10,28 @@ import org.koin.test.assertHasNoStandaloneInstance
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+/**
+ * `customKoin` can not be a member of the companion object,
+ * cause companion objects are global (shared by all threads)
+ * and such objects are frozen by default in kotlin/native.
+ *
+ * Therefore, all members of the koinApplication are also frozen,
+ * including the `SingleDefinitionInstance` holding `Simple.ComponentA`.
+ *
+ * Because `SingleDefinitionInstance` contains a `var value`,
+ * that will be mutated when the instance is lazily created,
+ * a `kotlin.native.concurrent.InvalidMutabilityException` will be thrown
+ * when the application is stored in a frozen object.
+ */
+private val customKoin = koinApplication {
+    modules(module {
+        single { Simple.ComponentA() }
+    })
+}.koin
+
 abstract class CustomKoinComponent : KoinComponent {
 
     override fun getKoin(): Koin = customKoin
-
-    companion object {
-        val customKoin = koinApplication {
-            modules(module {
-                single { Simple.ComponentA() }
-            })
-        }.koin
-    }
 }
 
 class MyCustomApp : CustomKoinComponent() {
@@ -32,7 +43,7 @@ class CustomKoinComponentTest {
     @Test
     fun `can inject KoinComponent from custom instance`() {
         val app = MyCustomApp()
-        val a = CustomKoinComponent.customKoin.get<Simple.ComponentA>()
+        val a = customKoin.get<Simple.ComponentA>()
 
         assertEquals(app.a, a)
 
