19ea6a05ac417384429e0b42587812b5678a259a
Andreas Schattney <a.schattney@gmail.com>
2019-10-04 21:00:16 +0200

    Fix koin-core-ext api for ScopeSets

--- koin-projects/koin-core-ext/src/main/kotlin/org/koin/experimental/builder/ScopeSetExt.kt
+++ koin-projects/koin-core-ext/src/main/kotlin/org/koin/experimental/builder/ScopeSetExt.kt
@@ -4,6 +4,7 @@ import org.koin.core.definition.BeanDefinition
 import org.koin.core.definition.Options
 import org.koin.core.definition.createFactory
 import org.koin.core.definition.createScoped
+import org.koin.core.definition.Kind
 import org.koin.core.error.DefinitionOverrideException
 import org.koin.core.qualifier.Qualifier
 import org.koin.core.scope.Scope
@@ -15,18 +16,10 @@ import org.koin.dsl.ScopeSet
  * @param createOnStart - need to be created at start
  * @param override - allow definition override
  */
-inline fun <S: Scope, reified T : Any> ScopeSet<S>.scoped(
+inline fun <reified T : Any> ScopeSet<*>.scoped(
         name: Qualifier? = null,
         override: Boolean = false
-): BeanDefinition<S, T> {
-    val beanDefinition = this.definitionFactory.createScoped(name, qualifier) { create<T>() }
-    declareDefinition(beanDefinition, Options(false, override))
-    val added = definitions.add(beanDefinition)
-    if (!added) {
-        throw DefinitionOverrideException("Can't add definition $beanDefinition as it already exists")
-    }
-    return beanDefinition
-}
+): BeanDefinition<out Scope, T> = internal(name, override, Kind.Scoped)
 
 /**
  * Create a Factory definition for given type T
@@ -34,18 +27,10 @@ inline fun <S: Scope, reified T : Any> ScopeSet<S>.scoped(
  * @param qualifier
  * @param override - allow definition override
  */
-inline fun <S: Scope, reified T> ScopeSet<S>.factory(
+inline fun <reified T: Any> ScopeSet<*>.factory(
         name: Qualifier? = null,
         override: Boolean = false
-): BeanDefinition<S, T> {
-    val beanDefinition = this.definitionFactory.createFactory(name, qualifier) { create<T>() }
-    declareDefinition(beanDefinition, Options(false, override))
-    val added = definitions.add(beanDefinition)
-    if (!added) {
-        throw DefinitionOverrideException("Can't add definition $beanDefinition as it already exists")
-    }
-    return beanDefinition
-}
+): BeanDefinition<out Scope, T> = internal(name, override, Kind.Factory)
 
 /**
  * Create a Single definition for given type T to modules and cast as R
@@ -53,18 +38,10 @@ inline fun <S: Scope, reified T> ScopeSet<S>.factory(
  * @param createOnStart - need to be created at start
  * @param override - allow definition override
  */
-inline fun <S: Scope, reified R : Any, reified T : R> ScopeSet<S>.scopedBy(
+inline fun <reified R : Any, reified T : R> ScopeSet<*>.scopedBy(
         name: Qualifier? = null,
         override: Boolean = false
-): BeanDefinition<S, R> {
-    val beanDefinition = this.definitionFactory.createScoped(name, qualifier) { create<T>() as R }
-    declareDefinition(beanDefinition, Options(false, override))
-    val added = definitions.add(beanDefinition)
-    if (!added) {
-        throw DefinitionOverrideException("Can't add definition $beanDefinition as it already exists")
-    }
-    return beanDefinition
-}
+): BeanDefinition<out Scope, R> = internalBy(name, override, Kind.Scoped)
 
 /**
  * Create a Factory definition for given type T to modules and cast as R
@@ -76,7 +53,27 @@ inline fun <S: Scope, reified R : Any, reified T : R> ScopeSet<S>.factoryBy(
         name: Qualifier? = null,
         override: Boolean = false
 ): BeanDefinition<S, R> {
-    val beanDefinition = this.definitionFactory.createFactory(name, qualifier) { create<T>() as R }
+    return internalBy(name, override, Kind.Factory)
+}
+
+@PublishedApi
+internal inline fun <S: Scope, reified T: Any> ScopeSet<S>.internal(
+        name: Qualifier?,
+        override: Boolean,
+        kind: Kind
+): BeanDefinition<S, T> = internalBy(name, override, kind)
+
+@PublishedApi
+internal inline fun <S: Scope, reified R : Any, reified T : R> ScopeSet<S>.internalBy(
+        name: Qualifier?,
+        override: Boolean,
+        kind: Kind
+): BeanDefinition<S, R> {
+    val beanDefinition = if (kind == Kind.Scoped) {
+        this.definitionFactory.createScoped(name, qualifier) { create<T>() as R }
+    } else {
+        this.definitionFactory.createFactory(name, qualifier) { create<T>() as R }
+    }
     declareDefinition(beanDefinition, Options(false, override))
     val added = definitions.add(beanDefinition)
     if (!added) {
