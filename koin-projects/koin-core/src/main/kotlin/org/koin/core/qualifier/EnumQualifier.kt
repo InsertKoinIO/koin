870ed587a920446dceda8052901f5ee54179dec9
Julian Bell <jerbell@gmail.com>
2019-10-05 07:59:20 +0800

    Change sample code

--- koin-projects/koin-core/src/main/kotlin/org/koin/core/qualifier/EnumQualifier.kt
+++ koin-projects/koin-core/src/main/kotlin/org/koin/core/qualifier/EnumQualifier.kt
@@ -4,8 +4,8 @@ package org.koin.core.qualifier
  * Class which allows enums to be used as Qualifiers.
  * More strict than strings and are better for refactoring.
  * @author Julian Bell
- * @sample get(named(Direction.NORTH))
- * @sample inject(named(Direction.SOUTH))
+ * @sample get(qualifier(Direction.NORTH))
+ * @sample inject(qualifier(Direction.SOUTH))
  */
 data class EnumQualifier<E : Enum<E>>(val value: E) : Qualifier {
     override fun toString(): String {
