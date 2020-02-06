fff580c3bd61a42a2981c97b9abe2937437da961
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2020-02-06 18:30:17 +0100

    fix header

--- koin-projects/koin-core/src/main/kotlin/org/koin/core/context/GlobalContext.kt
+++ koin-projects/koin-core/src/main/kotlin/org/koin/core/context/GlobalContext.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 the original author or authors.
+ * Copyright 2017-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@ import org.koin.core.error.KoinAppAlreadyStartedException
  *
  * Support to help inject automatically instances once KoinApp has been started
  *
+ * @author Arnaud Giuliani
  */
 class GlobalContext : KoinContext {
 
--- koin-projects/koin-core/src/main/kotlin/org/koin/core/context/KoinContextHandler.kt
+++ koin-projects/koin-core/src/main/kotlin/org/koin/core/context/KoinContextHandler.kt
@@ -1,8 +1,26 @@
+/*
+ * Copyright 2017-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.koin.core.context
 
 import org.koin.core.Koin
 import org.koin.core.KoinApplication
 
+/**
+ * Help hold any implementation of KoinContext
+ */
 object KoinContextHandler {
 
     private var _context: KoinContext? = null
@@ -12,32 +30,36 @@ object KoinContextHandler {
     }
 
     /**
-     * StandAlone Koin App instance
+     * Retrieve current KoinContext
      */
     fun get(): Koin = getContext().get()
 
     /**
-     * StandAlone Koin App instance
+     * Retrieve current KoinContext or null
      */
     fun getOrNull(): Koin? = _context?.getOrNull()
 
-
-    fun register(koinContext: KoinContext) = synchronized(this){
-        if (_context != null){
+    /**
+     * Register new KoinContext
+     *
+     * @throws IllegalStateException if already registered
+     */
+    fun register(koinContext: KoinContext) = synchronized(this) {
+        if (_context != null) {
             error("A KoinContext is already started")
         }
         _context = koinContext
     }
 
     /**
-     * Start a Koin Application as StandAlone
+     * Start a Koin Application on current KoinContext
      */
     fun start(koinApplication: KoinApplication) {
         getContext().setup(koinApplication)
     }
 
     /**
-     * Stop current StandAlone Koin application
+     * Stop current KoinContext & clear it
      */
     fun stop() {
         _context?.stop()
