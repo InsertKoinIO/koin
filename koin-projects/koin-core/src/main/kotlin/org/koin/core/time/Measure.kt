2a66a7b06037c97a9af9384fb5dddc94749024b5
Arnaud Giuliani <arnaud.giuliani@blablacar.com>
2020-03-04 22:57:40 +0100

    wip

--- koin-projects/koin-core/src/main/kotlin/org/koin/core/time/Measure.kt
+++ koin-projects/koin-core/src/main/kotlin/org/koin/core/time/Measure.kt
@@ -12,10 +12,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.koin.core.time
+ */
+package org.koin.core.time
 
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
+
 
 /**
  * Measure functions
