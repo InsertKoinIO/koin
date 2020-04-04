cbc93f7b56682898673b2aeda32e007a43ca2c16
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2020-04-04 11:58:21 +0200

    fixing scripts

--- koin-projects/install.sh
+++ koin-projects/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+./gradlew :koin-core-ext:install :koin-gradle-plugin:install :koin-ktor:install :koin-logger-slf4j:install
+
 ./gradlew publishToMavenLocal -Dorg.gradle.parallel=false
 
