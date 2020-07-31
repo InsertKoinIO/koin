4c61f981074106e2e24ee099547dfff1e2ac641f
Marko Pukari <marko.pukari@nitorcreations.com>
2020-07-31 10:09:38 +0300

    Feature/junit5 support (#784)
    * Separate junit4 from koin test module
    
    * Koin TestRule verification
    
    * junit5 extensions
    
    * Rename koin-test -> koin-test-core
    
    No need to duplicate the test-core functionality to junit4 and junit5
    
    * Rename koin-test-junit4 -> koin-test to ensure backwards compatibility
    
    * Move junit4 Rule tests from koin-test-core to koin-test
    
    They where in the wrong place since the core is common for junit4 and junit5
    
    * JUnit5 documentation
    
    Co-authored-by: Marko Pukari <marko.pukari@mavericks.fi>

---
+++ koin-projects/koin-test-core/src/test/kotlin/org/koin/test/Components.kt
@@ -0,0 +1,21 @@
+package org.koin.test
+
+import org.koin.core.qualifier.Qualifier
+import java.util.*
+
+@Suppress("unused")
+class Simple {
+    class ComponentA
+    class ComponentB(val a: ComponentA)
+    class ComponentC(val b: ComponentB)
+    class MyString(val s: String)
+
+    class UUIDComponent {
+        fun getUUID() = UUID.randomUUID().toString()
+    }
+}
+
+object UpperCase : Qualifier {
+    override val value: String = "UpperCase"
+
+}
--- koin-projects/koin-test-core/src/test/kotlin/org/koin/test/KoinQualifierTest.kt
+++ koin-projects/koin-test-core/src/test/kotlin/org/koin/test/KoinQualifierTest.kt
@@ -3,11 +3,12 @@ package org.koin.test
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 
-class KoinQualifierTest : AutoCloseKoinTest() {
+class KoinQualifierTest : KoinTest {
 
     val qualifier = named("42")
     val myString42: Simple.MyString by inject(qualifier)
@@ -16,13 +17,15 @@ class KoinQualifierTest : AutoCloseKoinTest() {
     fun `use qualifier`() {
         startKoin {
             modules(
-                module {
-                    single(qualifier) { Simple.MyString("42") }
-                }
+                    module {
+                        single(qualifier) { Simple.MyString("42") }
+                    }
             )
         }
 
         assertNotNull(myString42)
+
+        stopKoin()
     }
 
 }
