191d356f6c78699f627bb255c5476485f0f6d7bc
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2020-09-08 18:35:12 +0200

    Change Time API

--- koin-projects/koin-core/src/commonMain/kotlin/org/koin/core/time/Measure.kt
+++ koin-projects/koin-core/src/commonMain/kotlin/org/koin/core/time/Measure.kt
@@ -17,6 +17,8 @@ package org.koin.core.time
 
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
+import kotlin.time.measureTime
+import kotlin.time.measureTimedValue
 
 
 /**
@@ -30,13 +32,9 @@ import kotlin.time.TimeSource
  */
 @OptIn(ExperimentalTime::class)
 fun measureDuration(code: () -> Unit): Double {
-    val clock = TimeSource.Monotonic
-    val mark = clock.markNow()
-    code()
-    return mark.elapsedNow().inMilliseconds
+    return measureTime(code).inMilliseconds
 }
 
-@OptIn(ExperimentalTime::class)
 fun measureDuration(message: String, code: () -> Unit) {
     val time = measureDuration(code)
     println("$message - $time ms")
@@ -47,13 +45,10 @@ fun measureDuration(message: String, code: () -> Unit) {
  */
 @OptIn(ExperimentalTime::class)
 fun <T> measureDurationForResult(code: () -> T): Pair<T, Double> {
-    val clock = TimeSource.Monotonic
-    val mark = clock.markNow()
-    val result = code()
-    return Pair(result, mark.elapsedNow().inMilliseconds)
+    val result = measureTimedValue(code)
+    return Pair(result.value, result.duration.inMilliseconds)
 }
 
-@OptIn(ExperimentalTime::class)
 fun <T> measureDurationForResult(message: String, code: () -> T): T {
     val (result, time) = measureDurationForResult(code)
     println("$message - $time ms")
