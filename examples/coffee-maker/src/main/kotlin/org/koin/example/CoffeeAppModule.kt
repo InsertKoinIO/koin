fd1ca53f8aa020a22c9b740dc0242dba2033ac62
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2021-05-19 21:03:18 +0200

    clean up

--- examples/coffee-maker/src/main/kotlin/org/koin/example/CoffeeAppModule.kt
+++ examples/coffee-maker/src/main/kotlin/org/koin/example/CoffeeAppModule.kt
@@ -4,14 +4,14 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 import org.koin.dsl.single
 
-// val coffeeAppModule = module {
-//    single { CoffeeMaker(get(), get()) }
-//    single<Pump> { Thermosiphon(get()) }
-//    single<Heater> { ElectricHeater() }
-// }
-
 val coffeeAppModule = module {
-    single<CoffeeMaker>()
-    single<Thermosiphon>() bind Pump::class
-    single<ElectricHeater>() bind Heater::class
+    single { CoffeeMaker(get(), get()) }
+    single<Pump> { Thermosiphon(get()) }
+    single<Heater> { ElectricHeater() }
 }
+
+// val coffeeAppModule = module {
+//     single<CoffeeMaker>()
+//     single<Thermosiphon>() bind Pump::class
+//     single<ElectricHeater>() bind Heater::class
+// }
