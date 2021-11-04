6b367837f8536611379ba1eb1e3f50fa0bf3fbfd
Marcello Galhardo <marcello.galhardo@gmail.com>
2021-11-04 16:45:17 +0100

    Add qualifier support

--- android/koin-android/src/main/java/org/koin/androidx/viewmodel/dsl/ModuleViewModelExt.kt
+++ android/koin-android/src/main/java/org/koin/androidx/viewmodel/dsl/ModuleViewModelExt.kt
@@ -2,6 +2,7 @@ package org.koin.androidx.viewmodel.dsl
 
 import androidx.lifecycle.ViewModel
 import org.koin.core.module.Module
+import org.koin.core.qualifier.Qualifier
 import org.koin.core.scope.new
 
 /**
@@ -19,100 +20,111 @@ import org.koin.core.scope.new
  *
  * @author Marcello Galhardo
  *
- * @see new
+ * @see viewModel
  */
 inline fun <reified R : ViewModel> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: () -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2, reified T3> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2, T3) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2, reified T3, reified T4> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2, T3, T4) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2, reified T3, reified T4, reified T5> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2, T3, T4, T5) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2, T3, T4, T5, T6) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2, T3, T4, T5, T6, T7) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
 
 /**
- * @see factory
+ * @see viewModel
  */
 inline fun <reified R : ViewModel, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10> Module.viewModel(
+    qualifier: Qualifier,
     crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) -> R,
 ) {
-    viewModel(definition = { new(constructor) })
+    viewModel(qualifier = qualifier, definition = { new(constructor) })
 }
