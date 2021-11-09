43574dcae258627849a76e1832b8095ce35a4560
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2021-11-09 10:00:15 +0100

    Fix parameter stack clear after nullable parameter resolution in graph - fix #149

--- core/koin-core/src/commonMain/kotlin/org/koin/core/scope/Scope.kt
+++ core/koin-core/src/commonMain/kotlin/org/koin/core/scope/Scope.kt
@@ -162,10 +162,10 @@ data class Scope(
         return try {
             get(clazz, qualifier, parameters)
         } catch (e: ClosedScopeException) {
-            _koin.logger.debug("Scope closed - no instance found for ${clazz.getFullName()} on scope ${toString()}")
+            _koin.logger.debug("|- Scope closed - no instance found for ${clazz.getFullName()} on scope ${toString()}")
             null
         } catch (e: NoBeanDefFoundException) {
-            _koin.logger.debug("No instance found for ${clazz.getFullName()} on scope ${toString()}")
+            _koin.logger.debug("|- No instance found for ${clazz.getFullName()} on scope ${toString()}")
             null
         }
     }
@@ -225,12 +225,14 @@ data class Scope(
         }
         val parameters = parameterDef?.invoke()
         if (parameters != null) {
+            _koin.logger.log(Level.DEBUG) { "| put parameters on stack $parameters " }
             _parameterStack.addFirst(parameters)
         }
         val instanceContext = InstanceContext(_koin, this, parameters)
         val value = resolveValue<T>(qualifier, clazz, instanceContext, parameterDef)
         if (parameters != null) {
-            _parameterStack.removeFirst()
+            _koin.logger.log(Level.DEBUG) { "| remove parameters from stack" }
+            _parameterStack.removeFirstOrNull()
         }
         return value
     }
@@ -242,11 +244,11 @@ data class Scope(
         parameterDef: ParametersDefinition?
     ) = (_koin.instanceRegistry.resolveInstance(qualifier, clazz, this.scopeQualifier, instanceContext)
         ?: run {
-            _koin.logger.log(Level.DEBUG) { "'${clazz.getFullName()}' - q:'$qualifier' look in injected parameters" }
+            _koin.logger.log(Level.DEBUG) { "- lookup? t:'${clazz.getFullName()}' - q:'$qualifier' look in injected parameters" }
             _parameterStack.firstOrNull()?.getOrNull<T>(clazz)
         }
         ?: run {
-            _koin.logger.log(Level.DEBUG) { "'${clazz.getFullName()}' - q:'$qualifier' look at scope source" }
+            _koin.logger.log(Level.DEBUG) { "- lookup? t:'${clazz.getFullName()}' - q:'$qualifier' look at scope source" }
             _source?.let {
                 if (clazz.isInstance(it)) {
                     _source as? T
@@ -254,12 +256,12 @@ data class Scope(
             }
         }
         ?: run {
-            _koin.logger.log(Level.DEBUG) { "'${clazz.getFullName()}' - q:'$qualifier' look in other scopes" }
+            _koin.logger.log(Level.DEBUG) { "- lookup? t:'${clazz.getFullName()}' - q:'$qualifier' look in other scopes" }
             findInOtherScope<T>(clazz, qualifier, parameterDef)
         }
         ?: run {
-            _koin.logger.log(Level.DEBUG) { "'${clazz.getFullName()}' - q:'$qualifier' not found" }
             _parameterStack.clear()
+            _koin.logger.log(Level.DEBUG) { "| clear parameter stack" }
             throwDefinitionNotFound(qualifier, clazz)
         })
 
@@ -291,7 +293,7 @@ data class Scope(
     ): Nothing {
         val qualifierString = qualifier?.let { " & qualifier:'$qualifier'" } ?: ""
         throw NoBeanDefFoundException(
-            "No definition found for class:'${clazz.getFullName()}'$qualifierString. Check your definitions!"
+            "|- No definition found for class:'${clazz.getFullName()}'$qualifierString. Check your definitions!"
         )
     }
 
