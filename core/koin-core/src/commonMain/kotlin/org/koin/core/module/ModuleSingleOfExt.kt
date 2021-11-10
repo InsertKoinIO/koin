51616e331a3f8d8f119a3d7f1bcf26106ab39cdf
Marcello Galhardo <marcello.galhardo@gmail.com>
2021-11-10 09:02:27 +0100

    Reorder parameters of `*Of` functions for scope

--- core/koin-core/src/commonMain/kotlin/org/koin/core/module/ModuleSingleOfExt.kt
+++ core/koin-core/src/commonMain/kotlin/org/koin/core/module/ModuleSingleOfExt.kt
@@ -37,23 +37,8 @@ import org.koin.core.scope.new
  * @see new
  */
 inline fun <reified R> Module.singleOf(
-    qualifier: Qualifier,
     crossinline constructor: () -> R,
-): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R> Module.singleOf(
-    crossinline constructor: () -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -61,14 +46,7 @@ inline fun <reified R, reified T1> Module.singleOf(
  */
 inline fun <reified R, reified T1> Module.singleOf(
     crossinline constructor: (T1) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -76,14 +54,7 @@ inline fun <reified R, reified T1, reified T2> Module.singleOf(
  */
 inline fun <reified R, reified T1, reified T2> Module.singleOf(
     crossinline constructor: (T1, T2) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2, reified T3> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2, T3) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -91,14 +62,7 @@ inline fun <reified R, reified T1, reified T2, reified T3> Module.singleOf(
  */
 inline fun <reified R, reified T1, reified T2, reified T3> Module.singleOf(
     crossinline constructor: (T1, T2, T3) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2, reified T3, reified T4> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2, T3, T4) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -106,14 +70,7 @@ inline fun <reified R, reified T1, reified T2, reified T3, reified T4> Module.si
  */
 inline fun <reified R, reified T1, reified T2, reified T3, reified T4> Module.singleOf(
     crossinline constructor: (T1, T2, T3, T4) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2, T3, T4, T5) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -121,14 +78,7 @@ inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T
  */
 inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5> Module.singleOf(
     crossinline constructor: (T1, T2, T3, T4, T5) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2, T3, T4, T5, T6) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -136,14 +86,7 @@ inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T
  */
 inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6> Module.singleOf(
     crossinline constructor: (T1, T2, T3, T4, T5, T6) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -151,14 +94,7 @@ inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T
  */
 inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7> Module.singleOf(
     crossinline constructor: (T1, T2, T3, T4, T5, T6, T7) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -166,14 +102,7 @@ inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T
  */
 inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8> Module.singleOf(
     crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -181,14 +110,7 @@ inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T
  */
 inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9> Module.singleOf(
     crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
-
-/**
- * @see singleOf
- */
-inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10> Module.singleOf(
-    qualifier: Qualifier,
-    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) -> R,
+    qualifier: Qualifier? = null,
 ): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
 
 /**
@@ -196,4 +118,5 @@ inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T
  */
 inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10> Module.singleOf(
     crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) -> R,
-): Pair<Module, InstanceFactory<R>> = single { new(constructor) }
+    qualifier: Qualifier? = null,
+): Pair<Module, InstanceFactory<R>> = single(qualifier) { new(constructor) }
