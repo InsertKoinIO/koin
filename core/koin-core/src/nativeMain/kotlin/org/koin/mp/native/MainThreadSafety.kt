122e980fa9a912894a57e8233f37dba8f91317f8
Kevin Galligan <kgalligan@gmail.com>
2021-12-24 15:20:44 -0500

    Clean up and revert some things
    Signed-off-by: Kevin Galligan <kgalligan@gmail.com>

--- core/koin-core/src/nativeMain/kotlin/org/koin/mp/native/MainThreadSafety.kt
+++ core/koin-core/src/nativeMain/kotlin/org/koin/mp/native/MainThreadSafety.kt
@@ -4,20 +4,7 @@ import kotlinx.cinterop.StableRef
 import kotlin.native.concurrent.ensureNeverFrozen
 import kotlin.native.concurrent.freeze
 
-@OptIn(ExperimentalStdlibApi::class)
-internal fun <T : Any> createdGuardedValue(startVal: T):GuardedValue<T>{
-    return if(isExperimentalMM()){
-        AnyThreadValue(startVal)
-    }else{
-        MainThreadValue(startVal)
-    }
-}
-
-internal interface GuardedValue<T : Any> {
-    fun get(): T
-}
-
-internal class MainThreadValue<T : Any>(startVal: T) :GuardedValue<T>{
+class MainThreadValue<T : Any>(startVal: T) {
     private val stableRef = StableRef.create(startVal)
 
     init {
@@ -26,21 +13,13 @@ internal class MainThreadValue<T : Any>(startVal: T) :GuardedValue<T>{
         freeze()
     }
 
-    override fun get(): T {
+    fun get(): T {
         assertMainThread()
         return stableRef.get()
     }
 }
 
-internal class AnyThreadValue<T : Any>(private val startVal: T): GuardedValue<T>{
-    init {
-        startVal.ensureNeverFrozen()
-        ensureNeverFrozen()
-    }
-    override fun get(): T = startVal
-}
-
-internal expect val isMainThread: Boolean// = NSThread.isMainThread
+expect val isMainThread: Boolean// = NSThread.isMainThread
 
 internal fun assertMainThread() {
     if (!isMainThread) throw IllegalStateException("Must be main thread")
