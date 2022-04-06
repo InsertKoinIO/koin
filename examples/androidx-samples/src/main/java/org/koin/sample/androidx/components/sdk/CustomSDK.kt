ed5daaf745706204f5b314c085bc9d942727012a
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2022-04-06 18:56:43 +0200

    add more test about createAtStart & fix koinApplication to handle createAtStart - fix #1248

--- examples/androidx-samples/src/main/java/org/koin/sample/androidx/components/sdk/CustomSDK.kt
+++ examples/androidx-samples/src/main/java/org/koin/sample/androidx/components/sdk/CustomSDK.kt
@@ -9,6 +9,8 @@ import org.koin.dsl.koinApplication
 import org.koin.dsl.module
 import org.koin.sample.androidx.components.main.SimpleService
 import org.koin.sample.androidx.components.main.SimpleServiceImpl
+import org.koin.sample.androidx.components.scope.Session
+import org.koin.sample.androidx.sdk.HostActivity
 
 // Custom Koin instance Holder
 object CustomSDK {
@@ -22,6 +24,9 @@ val sdkModule = module {
     single<SimpleService> { SimpleServiceImpl() }
     single { SDKService() }
     viewModel { SDKVIewModel(get()) }
+    scope<HostActivity>{
+        Session()
+    }
 }
 
 // Custom KoinComponent
--- examples/androidx-samples/src/main/java/org/koin/sample/androidx/sdk/HostActivity.kt
+++ examples/androidx-samples/src/main/java/org/koin/sample/androidx/sdk/HostActivity.kt
@@ -4,24 +4,36 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.host_activity.*
 import org.koin.android.ext.android.inject
+import org.koin.androidx.scope.activityScope
+import org.koin.core.component.KoinScopeComponent
+import org.koin.core.context.GlobalContext
+import org.koin.core.scope.Scope
 import org.koin.sample.android.R
 import org.koin.sample.androidx.components.main.SimpleService
+import org.koin.sample.androidx.components.scope.Session
+import org.koin.sample.androidx.components.sdk.CustomKoinComponent
 import org.koin.sample.androidx.components.sdk.CustomSDK
 import org.koin.sample.androidx.components.sdk.CustomService
 import org.koin.sample.androidx.components.sdk.SDKActivity
 import org.koin.sample.androidx.utils.navigateTo
 
-class HostActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity(), CustomKoinComponent, KoinScopeComponent {
+
+    override val scope : Scope by activityScope()
 
     // Inject by Interface - default definition
-    val service: SimpleService by inject()
+    val sdkService: SimpleService by inject()
+    val sdkSession : Session by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val sdkService = CustomSDK.koinApp.koin.get<SimpleService>()
-        assert(service != sdkService)
+        val koinSDK = CustomSDK.koinApp.koin
+
+        val globalService = GlobalContext.get().get<SimpleService>()//CustomSDK.koinApp.koin.get<SimpleService>()
+        assert(sdkService != globalService)
         assert(CustomService().service == sdkService)
+        assert(sdkSession != null)
 
         title = "Host Activity with SDK"
         setContentView(R.layout.host_activity)
