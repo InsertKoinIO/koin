c9faa9e4001650a15d7418c7f5140da81a3f353b
pedrofsn <pedrokra@gmail.com>
2022-10-16 11:50:15 -0300

    Adding module in helper

--- android/koin-android/src/test/java/org/koin/test/android/helper/Helper.kt
+++ android/koin-android/src/test/java/org/koin/test/android/helper/Helper.kt
@@ -3,6 +3,8 @@ package org.koin.test.android.helper
 import android.content.ComponentCallbacks
 import android.content.res.Configuration
 import org.koin.core.component.KoinComponent
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 
 object Helper {
     val koinComponent: KoinComponent = object : ComponentCallbacks, KoinComponent {
@@ -13,5 +15,10 @@ object Helper {
     val componentCallbacks = object : ComponentCallbacks {
         override fun onConfigurationChanged(newConfig: Configuration) = Unit
         override fun onLowMemory() = Unit
+    }
+
+    val module = module {
+        single<FakeContent>(named("custom")) { FakeContentImpl() }
+        single<FakeContent> { FakeContentImpl() }
     }
 }
