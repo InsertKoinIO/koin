da15fc89cc1ca595ac6c95586f23bf67be767f3e
pedrofsn <pedrokra@gmail.com>
2022-10-16 18:20:57 -0300

    Unit test covaage for KoinExt as 100%

--- android/koin-android/src/test/java/org/koin/test/android/ext/koin/KoinExtTest.kt
+++ android/koin-android/src/test/java/org/koin/test/android/ext/koin/KoinExtTest.kt
@@ -2,10 +2,13 @@ package org.koin.test.android.ext.koin
 
 import android.app.Application
 import android.content.Context
+import io.mockk.every
 import io.mockk.mockk
+import java.lang.Exception
 import org.junit.Assert
 import org.junit.Test
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
 import org.koin.android.ext.koin.androidLogger
 import org.koin.android.logger.AndroidLogger
 import org.koin.core.KoinApplication
@@ -13,6 +16,7 @@ import org.koin.core.annotation.KoinInternalApi
 import org.koin.core.logger.Level
 import org.koin.core.logger.Logger
 import org.koin.core.logger.MESSAGE
+import org.koin.core.registry.saveProperties
 
 class KoinExtTest {
 
@@ -112,4 +116,53 @@ class KoinExtTest {
         Assert.assertEquals(key, "android.content.Context::_root_")
         Assert.assertTrue(value.beanDefinition.primaryType == Context::class)
     }
+
+    @OptIn(KoinInternalApi::class)
+    @Test
+    fun `GIVEN KoinApplication WHEN call android properties THEN result as expected`() {
+        // GIVEN
+        val koinApplication = KoinApplication.init()
+        val exception = Exception("exception")
+        val customLogger = object : Logger(Level.INFO) {
+            override fun log(level: Level, msg: MESSAGE) {
+                // THEN
+                when {
+                    msg.contains("[init]") -> {
+                        Assert.assertTrue(Level.INFO == level)
+                        Assert.assertEquals("[init] declare Android Context", msg)
+                    }
+                    msg.contains("no assets") -> {
+                        Assert.assertTrue(Level.INFO == level)
+                        Assert.assertEquals("[Android-Properties] no assets/koin.properties file to load", msg)
+                    }
+                    msg.contains("loaded") -> {
+                        Assert.assertTrue(Level.INFO == level)
+                        Assert.assertEquals("[Android-Properties] loaded kotlin.Unit properties from assets/koin.properties", msg)
+                        every { koinApplication.koin.propertyRegistry.saveProperties(any()) } throws exception
+                    }
+                    msg.contains("[Android-Properties] error for") -> {
+                        Assert.assertTrue(Level.ERROR == level)
+                        Assert.assertTrue(msg.contains("[Android-Properties] error for binding properties : "))
+                        every { koinApplication.koin.get<Context>() } throws exception
+                    }
+                    msg.contains("[Android-Properties] error while") -> {
+                        Assert.assertTrue(Level.ERROR == level)
+                        Assert.assertTrue(msg.contains("[Android-Properties] error while loading properties from assets/koin.properties : "))
+                    }
+                }
+            }
+        }
+        val context = mockk<Context>(relaxed = true) {
+            every { assets?.list("") } returns arrayOf("koin.properties")
+        }
+        koinApplication.koin.setupLogger(customLogger)
+        koinApplication.androidContext(context)
+
+        // WHEN
+        koinApplication.androidFileProperties(koinPropertyFile = "koin.properties")
+        koinApplication.androidFileProperties(koinPropertyFile = "koin.properties")
+        koinApplication.androidFileProperties()
+    }
+
+
 }
