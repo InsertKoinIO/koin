3b6449513969fa0c9b7ce0562bb73dd52e78d446
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2022-12-13 17:24:56 +0100

    add close test

---
+++ core/koin-core/src/commonTest/kotlin/org/koin/dsl/CloseDefinitionTest.kt
@@ -0,0 +1,54 @@
+package org.koin.dsl
+
+import org.koin.Simple
+import org.koin.core.logger.Level
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CloseDefinitionTest {
+
+    @Test
+    fun test_onClose(){
+        var closed = false
+
+        val koin = koinApplication {
+            modules(
+                module {
+                    single { Simple.ComponentA() } onClose {
+                        closed = true
+                    }
+                }
+            )
+        }.koin
+
+        koin.close()
+        assertTrue(closed)
+    }
+
+    @Test
+    fun test_onClose_from_unload(){
+        var closed = false
+
+        val module = module {
+            single { Simple.ComponentA() } onClose {
+                closed = !closed
+                println("closing ComponentA - closed = $closed")
+            }
+        }
+
+        val koin = koinApplication {
+            printLogger(Level.DEBUG)
+            modules(
+                module
+            )
+        }.koin
+
+        assertTrue(!closed)
+        koin.unloadModules(listOf(module))
+        assertTrue(closed)
+        koin.loadModules(listOf(module))
+        koin.unloadModules(listOf(module))
+        assertTrue(!closed)
+    }
+
+}
