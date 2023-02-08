63b02f999c365fba4195e111635574974e40efd6
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2023-02-08 16:34:53 +0100

    fix build for tests

--- examples/coffee-maker/src/test/kotlin/org/koin/example/CoffeeMakerTest.kt
+++ examples/coffee-maker/src/test/kotlin/org/koin/example/CoffeeMakerTest.kt
@@ -5,41 +5,47 @@ import io.mockk.mockkClass
 import io.mockk.verifySequence
 import org.junit.Rule
 import org.junit.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.core.logger.Level
 import org.koin.test.KoinTest
-import org.koin.test.KoinTestRule
 import org.koin.test.inject
 import org.koin.test.mock.MockProviderRule
 import org.koin.test.mock.declareMock
+import kotlin.test.AfterTest
 
 class CoffeeMakerTest : KoinTest {
 
-   private val coffeeMaker: CoffeeMaker by inject()
-   private val heater: Heater by inject()
+    private val coffeeMaker: CoffeeMaker by inject()
+    private val heater: Heater by inject()
 
-   @get:Rule
-   val mockProvider = MockProviderRule.create { clazz ->
-       mockkClass(clazz, relaxed = true)
-   }
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz ->
+        mockkClass(clazz, relaxed = true)
+    }
 
-   @get:Rule
-   val koinTestRule = KoinTestRule.create {
-       printLogger(Level.DEBUG)
-       modules(coffeeAppModule)
-   }
+    @AfterTest
+    fun after() {
+        stopKoin()
+    }
 
-   @Test
-   fun testHeaterIsTurnedOnAndThenOff() {
-       declareMock<Heater> {
-           every { isHot() } returns true
-       }
+    @Test
+    fun testHeaterIsTurnedOnAndThenOff() {
+        startKoin {
+            printLogger(Level.DEBUG)
+            modules(coffeeAppModule)
+        }
 
-       coffeeMaker.brew()
+        declareMock<Heater> {
+            every { isHot() } returns true
+        }
 
-       verifySequence {
-           heater.on()
-           heater.isHot()
-           heater.off()
-       }
-   }
+        coffeeMaker.brew()
+
+        verifySequence {
+            heater.on()
+            heater.isHot()
+            heater.off()
+        }
+    }
 }
