58d8cab448cc72eca1398436ae332bd3b468715d
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2023-02-21 17:56:30 +0100

    Fix default qualifier parameter of worker()

--- android/koin-androidx-workmanager/src/main/java/org/koin/androidx/workmanager/dsl/ModuleExt.kt
+++ android/koin-androidx-workmanager/src/main/java/org/koin/androidx/workmanager/dsl/ModuleExt.kt
@@ -32,7 +32,7 @@ import org.koin.dsl.bind
  * @author Arnaud Giuliani
  **/
 inline fun <reified T : ListenableWorker> Module.worker(
-    qualifier: Qualifier = named<T>(),
+    qualifier: Qualifier? = null,
     noinline definition: Definition<T>
 ): KoinDefinition<T> {
     val factory = factory(qualifier, definition)
