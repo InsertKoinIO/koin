8ca591b53e75ad75e08eaf301559bef98db8b2f6
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2023-08-31 18:02:08 +0200

    Compose KoinIsolatedContext to help run child composables using a isolated Koin context

--- compose/sample-android-compose/src/main/java/org/koin/sample/androidx/compose/MainActivity.kt
+++ compose/sample-android-compose/src/main/java/org/koin/sample/androidx/compose/MainActivity.kt
@@ -3,13 +3,19 @@ package org.koin.sample.androidx.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import org.koin.android.ext.android.getKoin
 import org.koin.androidx.scope.ScopeActivity
+import org.koin.sample.androidx.compose.data.sdk.SDKData
 import java.util.logging.Logger
 
 class MainActivity : ScopeActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // ensure SDK is not accessible in main context
+        assert(getKoin().getOrNull<SDKData>() == null)
+
         setContent {
             MaterialTheme { App() }
         }
---
+++ compose/sample-android-compose/src/main/java/org/koin/sample/androidx/compose/data/sdk/SDKData.kt
@@ -0,0 +1,7 @@
+package org.koin.sample.androidx.compose.data.sdk
+
+import java.util.UUID
+
+class SDKData {
+    val id = UUID.randomUUID().toString()
+}
