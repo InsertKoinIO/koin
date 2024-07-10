13318ed2122e886d3a6c0c63bf4e99ab01aed571
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2024-07-10 18:14:03 +0200

    Comment equals/hashcode on beandefinition to let have modules with same definition, but different instance factory

--- projects/core/koin-core/src/commonTest/kotlin/org/koin/dsl/ModuleFactoryIsolationTest.kt
+++ projects/core/koin-core/src/commonTest/kotlin/org/koin/dsl/ModuleFactoryIsolationTest.kt
@@ -9,8 +9,7 @@ import kotlin.test.assertTrue
 
 val myValModule = module { single { Simple.ComponentA() } }
 
-val myGetModule : Module
-    get() = module { single { Simple.ComponentA() }  }
+val myGetModule : Module get() = module { single { Simple.ComponentA() }  }
 
 fun myFunModule() = module { single { Simple.ComponentA() }  }
 
@@ -37,4 +36,13 @@ class ModuleFactoryIsolationTest {
         assertEquals(aF.mappings.values.first().beanDefinition, bF.mappings.values.first().beanDefinition)
     }
 
+    @Test
+    fun testVariableIsolationAndInstanceFactoriesLocal(){
+        assertTrue(a.mappings != b.mappings)
+        assertEquals(a.mappings.values.first().beanDefinition, b.mappings.values.first().beanDefinition)
+    }
+
 }
+
+val ModuleFactoryIsolationTest.a : Module get() = myGetModule
+val ModuleFactoryIsolationTest.b : Module get() = myGetModule
