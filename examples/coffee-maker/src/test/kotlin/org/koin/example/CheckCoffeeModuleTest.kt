894d15d91537d2392edc2c897ecb63b90ecd24fd
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2024-07-24 17:15:42 +0200

    fixing tests - need migrate compose plugin

--- examples/coffee-maker/src/test/kotlin/org/koin/example/CheckCoffeeModuleTest.kt
+++ examples/coffee-maker/src/test/kotlin/org/koin/example/CheckCoffeeModuleTest.kt
@@ -1,11 +1,8 @@
 package org.koin.example
 
 import org.junit.Test
-import org.junit.experimental.categories.Category
-import org.koin.test.category.CheckModuleTest
 import org.koin.test.verify.verify
 
-@Category(CheckModuleTest::class)
 class CheckCoffeeModuleTest {
 
     @Test
