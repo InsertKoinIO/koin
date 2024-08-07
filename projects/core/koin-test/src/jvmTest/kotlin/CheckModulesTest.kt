ca8ec6e13f81d418a7ef5a75554178f06b6585d4
pedrofsn <pedrokra@gmail.com>
2024-08-07 14:48:44 -0300

    Adding jUnit tests

---
+++ projects/core/koin-test/src/jvmTest/kotlin/CheckModulesTest.kt
@@ -0,0 +1,34 @@
+import kotlin.test.Test
+import kotlin.test.fail
+import org.koin.core.error.InstanceCreationException
+import org.koin.dsl.module
+import org.koin.test.Simple
+import org.koin.test.check.checkKoinModules
+import org.koin.test.verify.MissingKoinDefinitionException
+
+class CheckModulesTest {
+
+    @Test
+    fun verify_one_simple_module() {
+        val module = module {
+            single { Simple.ComponentA() }
+            single { Simple.ComponentB(get()) }
+        }
+
+        try {
+            checkKoinModules(listOf(module))
+        } catch (e: MissingKoinDefinitionException) {
+            fail("Should not fail to verify module - $e")
+        }
+    }
+
+    @Test(expected = InstanceCreationException::class)
+    fun verify_one_simple_broken_module() {
+        val module = module {
+            single { Simple.ComponentB(get()) }
+        }
+
+        checkKoinModules(listOf(module))
+        fail("Should fail to verify module")
+    }
+}
