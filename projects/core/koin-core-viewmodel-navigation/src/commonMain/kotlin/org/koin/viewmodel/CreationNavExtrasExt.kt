5e8aa6837c6ae2be27dafeb4ea2304281d1d8e6d
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2024-08-30 17:39:26 +0200

    remove already overriden property from NavBackEntry

--- projects/core/koin-core-viewmodel-navigation/src/commonMain/kotlin/org/koin/viewmodel/CreationNavExtrasExt.kt
+++ projects/core/koin-core-viewmodel-navigation/src/commonMain/kotlin/org/koin/viewmodel/CreationNavExtrasExt.kt
@@ -6,9 +6,7 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.navigation.NavBackStackEntry
 import org.koin.core.annotation.KoinInternalApi
 
-@OptIn(KoinInternalApi::class)
 fun defaultNavExtras(viewModelStoreOwner: ViewModelStoreOwner): CreationExtras = when {
-    viewModelStoreOwner is NavBackStackEntry -> viewModelStoreOwner.defaultViewModelCreationExtras
     viewModelStoreOwner is HasDefaultViewModelProviderFactory -> viewModelStoreOwner.defaultViewModelCreationExtras
     else -> CreationExtras.Empty
 }
