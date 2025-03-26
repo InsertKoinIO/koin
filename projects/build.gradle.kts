2b68320272c1e868a1adefba170b47ce51b7ecdc
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2025-03-26 17:08:47 +0100

    setup detek

--- projects/build.gradle.kts
+++ projects/build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.kotlinMultiplatform).apply(false)
@@ -7,6 +9,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinBinary)
     alias(libs.plugins.nexusPublish)
+    alias(libs.plugins.detekt)
 }
 
 fun getRepositoryUsername(): String =
@@ -38,5 +41,21 @@ allprojects {
         dependsOn(dokkaHtml)
         archiveClassifier.set("javadoc")
         from(dokkaHtml.outputDirectory)
+    }
+
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        toolVersion = "1.23.8"
+//        config.setFrom(file("config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        reports {
+            xml.required.set(false)
+            html.required.set(true)
+            sarif.required.set(false)
+            md.required.set(true)
+        }
     }
 }
