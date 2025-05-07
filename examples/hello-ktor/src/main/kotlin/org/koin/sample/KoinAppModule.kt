69deadad37ba776b5f7a6c5aa2abe1677f59bab5
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2025-05-07 16:07:49 +0200

    fix demo app / disable tests for now

--- examples/hello-ktor/src/main/kotlin/org/koin/sample/KoinAppModule.kt
+++ examples/hello-ktor/src/main/kotlin/org/koin/sample/KoinAppModule.kt
@@ -1,15 +1,10 @@
 package org.koin.sample
 
-import org.koin.core.module.KoinDslMarker
-import org.koin.core.module.Module
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.createdAtStart
 import org.koin.core.module.dsl.scopedOf
 import org.koin.core.module.dsl.singleOf
-import org.koin.core.qualifier.TypeQualifier
-import org.koin.dsl.ScopeDSL
 import org.koin.dsl.module
-import org.koin.ktor.plugin.RequestScope
 import org.koin.module.requestScope
 
 val appModule = module {
