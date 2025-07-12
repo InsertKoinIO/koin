f32f9deb14adcfebc418b8e733c220f0fadd36b0
domgew <44265359+domgew@users.noreply.github.com>
2025-07-12 13:27:52 +0200

    Add failing component override tests with nested modules

--- projects/core/koin-core/src/commonTest/kotlin/org/koin/core/MultipleModuleDeclarationTest.kt
+++ projects/core/koin-core/src/commonTest/kotlin/org/koin/core/MultipleModuleDeclarationTest.kt
@@ -1,6 +1,10 @@
 package org.koin.core
 
 import org.koin.Simple
+import org.koin.core.component.KoinScopeComponent
+import org.koin.core.component.get
+import org.koin.core.component.getOrCreateScope
+import org.koin.core.scope.Scope
 import org.koin.dsl.koinApplication
 import org.koin.dsl.module
 import org.koin.test.assertDefinitionsCount
@@ -47,5 +51,217 @@ class MultipleModuleDeclarationTest {
         val b = koin.get<Simple.ComponentB>()
 
         assertEquals(a, b.a)
+    }
+
+    @Test
+    fun `resolve DI with several modules with override contract`() {
+        val expected = TestDependency(
+            name = "expected",
+        )
+
+        val app = koinApplication {
+            modules(
+                module {
+                    single {
+                        TestDependency(
+                            name = "a",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        TestDependency(
+                            name = "b",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        TestDependency(
+                            name = "c",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        expected
+                    }
+                },
+            )
+        }
+
+        val koin = app.koin
+        val actual = koin.get<TestDependency>()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `resolve DI with several nested modules with override contract`() {
+        val expected = TestDependency(
+            name = "expected",
+        )
+
+        val app = koinApplication {
+            modules(
+                module {
+                    includes(
+                        module {
+                            TestDependency(
+                                name = "a",
+                            )
+                        },
+                    )
+                    single {
+                        TestDependency(
+                            name = "a",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        TestDependency(
+                            name = "b",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        TestDependency(
+                            name = "c",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        expected
+                    }
+                },
+            )
+        }
+
+        val koin = app.koin
+        val actual = koin.get<TestDependency>()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `resolve DI with several nested modules from scope with override contract`() {
+        val expected = TestDependency(
+            name = "expected",
+        )
+
+        val app = koinApplication {
+            modules(
+                module {
+                    includes(
+                        module {
+                            TestDependency(
+                                name = "a",
+                            )
+                        },
+                    )
+                    single {
+                        TestDependency(
+                            name = "a",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        TestDependency(
+                            name = "b",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        TestDependency(
+                            name = "c",
+                        )
+                    }
+                },
+                module {
+                    single {
+                        expected
+                    }
+                },
+            )
+        }
+
+        val koin = app.koin
+        val scope = ModuleTestScope(koin)
+        val actual = scope.get<TestDependency>()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `resolve DI with several deeply nested modules from scope with override contract`() {
+        val expected = TestDependency(
+            name = "expected",
+        )
+
+        val app = koinApplication {
+            modules(
+                module {
+                    includes(
+                        module {
+                            includes(
+                                module {
+                                    TestDependency(
+                                        name = "a",
+                                    )
+                                },
+                            )
+                            single {
+                                TestDependency(
+                                    name = "a",
+                                )
+                            }
+                        },
+                        module {
+                            single {
+                                TestDependency(
+                                    name = "b",
+                                )
+                            }
+                        },
+                        module {
+                            single {
+                                TestDependency(
+                                    name = "c",
+                                )
+                            }
+                        },
+                        module {
+                            single {
+                                expected
+                            }
+                        },
+                    )
+                },
+            )
+        }
+
+        val koin = app.koin
+        val scope = ModuleTestScope(koin)
+        val actual = scope.get<TestDependency>()
+
+        assertEquals(expected, actual)
+    }
+
+    data class TestDependency(
+        val name: String,
+    )
+
+    class ModuleTestScope(
+        private val koin: Koin,
+    ) : KoinScopeComponent {
+        override val scope: Scope by getOrCreateScope()
+
+        override fun getKoin(): Koin =
+            koin
     }
 }
